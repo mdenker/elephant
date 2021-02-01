5da5e78a534389df089c5b055294567731ca1987
Simon Essink <essink@users.noreply.github.com>
2021-02-01 13:19:07 +0100

    Feature/time warping (#80)
    * use numba jit decorator
    
    * Sloppily catch division by zero
    
    * Add numba TODOs

--- elephant/time_warping.py
+++ elephant/time_warping.py
@@ -30,6 +30,7 @@ from neo import utils
 import numpy as np
 import quantities as pq
 import copy
+import numba
 
 __all__ = [
     "warp_sequence_of_time_points",
@@ -46,7 +47,9 @@ __all__ = [
 
 # TODO documentation
 # TODO speed up by parallelization
-
+# TODO put numba into elephant requirements
+# TODO take care of numba deprecation warning for untyped lists
+@numba.jit(nopython=True)
 def warp_sequence_of_time_points(sequence_of_time_points,
                                  original_time_knots,
                                  warping_time_knots):
@@ -88,6 +91,13 @@ def warp_sequence_of_time_points(sequence_of_time_points,
                       original_time_knots[knot_idx-1])
             y_diff = (warping_time_knots[knot_idx] -
                       warping_time_knots[knot_idx-1])
+        
+        # cover division by zero
+        if x_diff == 0:
+            print(x_diff, y_diff)
+            # TODO dig into this!
+            slope = 1
+        else:
             slope = y_diff / x_diff
 
         warped_sequence_of_time_points.append(
